0b78de77c578135555b9c3662782818e2a1e9975
Dhaval <dhvl.ptl123@yahoo.in>
2020-06-27 20:16:29 +0530

    added sample server

---
+++ packages/sample-server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = new express();
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+app.listen(3000, () => {
+    console.log("Started server at 3000");
+});
